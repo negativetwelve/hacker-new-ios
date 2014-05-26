c84931d6cddd8973841392fffda755c376f8c4b7
Mark Miyashita <negativetwelve@gmail.com>
2014-05-25 22:54:09 -0700

    Add base navigation controller

---
+++ HackerNews/HNNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  HNNavigationController.h
+//  HackerNews
+//
+//  Created by Mark Miyashita on 5/25/14.
+//  Copyright (c) 2014 Mark Miyashita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HNNavigationController : UINavigationController
+
+@end
---
+++ HackerNews/HNNavigationController.m
@@ -0,0 +1,35 @@
+//
+//  HNNavigationController.m
+//  HackerNews
+//
+//  Created by Mark Miyashita on 5/25/14.
+//  Copyright (c) 2014 Mark Miyashita. All rights reserved.
+//
+
+#import "HNNavigationController.h"
+
+@interface HNNavigationController ()
+
+@end
+
+@implementation HNNavigationController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+  self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+  if (self) {
+    // Custom initialization
+  }
+  return self;
+}
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end

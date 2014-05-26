a5b42f27721e1b4ac9ec28b76e2d9c107734261c
Mark Miyashita <negativetwelve@gmail.com>
2014-05-25 22:04:16 -0700

    Add hacker news top stories view controller

---
+++ HackerNews/HNTopStoriesViewController.h
@@ -0,0 +1,13 @@
+//
+//  HNTopStoriesViewController.h
+//  HackerNews
+//
+//  Created by Mark Miyashita on 5/25/14.
+//  Copyright (c) 2014 Mark Miyashita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HNTopStoriesViewController : HNViewController
+
+@end
---
+++ HackerNews/HNTopStoriesViewController.m
@@ -0,0 +1,13 @@
+//
+//  HNTopStoriesViewController.m
+//  HackerNews
+//
+//  Created by Mark Miyashita on 5/25/14.
+//  Copyright (c) 2014 Mark Miyashita. All rights reserved.
+//
+
+#import "HNTopStoriesViewController.h"
+
+@implementation HNTopStoriesViewController
+
+@end

b1e635d3a408a5642e8f45bec351c02f2448ff65
Mark Miyashita <negativetwelve@gmail.com>
2014-05-26 23:05:33 -0700

    Add recent stories view controller

---
+++ HackerNews/HNRecentStoriesViewController.h
@@ -0,0 +1,13 @@
+//
+//  HNRecentStoriesViewController.h
+//  HackerNews
+//
+//  Created by Mark Miyashita on 5/26/14.
+//  Copyright (c) 2014 Mark Miyashita. All rights reserved.
+//
+
+#import "HNViewController.h"
+
+@interface HNRecentStoriesViewController : HNViewController
+
+@end
---
+++ HackerNews/HNRecentStoriesViewController.m
@@ -0,0 +1,35 @@
+//
+//  HNRecentStoriesViewController.m
+//  HackerNews
+//
+//  Created by Mark Miyashita on 5/26/14.
+//  Copyright (c) 2014 Mark Miyashita. All rights reserved.
+//
+
+#import "HNRecentStoriesViewController.h"
+
+@interface HNRecentStoriesViewController ()
+
+@end
+
+@implementation HNRecentStoriesViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+  self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+  if (self) {
+    // Custom initialization
+  }
+  return self;
+}
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end

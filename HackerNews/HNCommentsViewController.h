92356e88095aa6ac0de61ae1fe651fcc96bfb2b1
Mark Miyashita <negativetwelve@gmail.com>
2014-05-27 20:18:05 -0700

    Add comments view controller

---
+++ HackerNews/HNCommentsViewController.h
@@ -0,0 +1,13 @@
+//
+//  HNCommentsViewController.h
+//  HackerNews
+//
+//  Created by Mark Miyashita on 5/27/14.
+//  Copyright (c) 2014 Mark Miyashita. All rights reserved.
+//
+
+#import "HNViewController.h"
+
+@interface HNCommentsViewController : HNViewController
+
+@end
---
+++ HackerNews/HNCommentsViewController.m
@@ -0,0 +1,38 @@
+//
+//  HNCommentsViewController.m
+//  HackerNews
+//
+//  Created by Mark Miyashita on 5/27/14.
+//  Copyright (c) 2014 Mark Miyashita. All rights reserved.
+//
+
+#import "HNCommentsViewController.h"
+
+@interface HNCommentsViewController ()
+
+@end
+
+@implementation HNCommentsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
